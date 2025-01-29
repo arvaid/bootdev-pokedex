de9bbfeb3a472aac291a2c82d2fff2822cd9c3cc
Dániel Árvai <arvai96@gmail.com>
2025-01-29 07:38:13 +0100

    cleanInput

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	slice := make([]string, 0)
+	text = strings.Trim(text, " ")
+	words := strings.Split(text, " ")
+	for _, word := range words {
+		word = strings.Trim(word, " ")
+		word = strings.ToLower(word)
+		if word == "" || word == " " {
+			continue
+		}
+		slice = append(slice, word)
+		fmt.Println(word)
+	}
+	return slice
 }
---
+++ repl_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("unequeal lengths: %d != %d", len(actual), len(c.expected))
+		}
+		// Check the length of the actual slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("%s != %s", word, expectedWord)
+			}
+		}
+	}
+}
